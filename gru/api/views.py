4bad2081a358ba6e1009169e740b365dc2b4251e
kameshwarasekar <kameshwarasekar@gmail.com>
2021-04-08 08:56:03 +0530

    Update views.py

--- gru/api/views.py
+++ gru/api/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from .models import Professor, University, Ranking
 from django.views import generic
+from django.http import HttpResponse
+from  django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Alumni
+from .serializers import AlumniSerializer
 
+# Create your views here.
+
+
+class AlumniList(APIView):
+    def get(self, request):
+        alumni = Alumni.objects.all()
+        serializer = AlumniSerializer(alumni, many=True)
+        return Response(serializer.data)
+    def post(self):
+        pass
 
 # Create your views here.
 def index(request):
